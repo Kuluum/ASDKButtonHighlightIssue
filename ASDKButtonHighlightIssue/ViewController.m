b9337d961e4240db94585eda0adc7b1fcdd26596
Daniil Nosov <DNosov@spbtv.com>
2017-03-05 17:02:53 +0300

    Initial commit with buggy sample project

--- ASDKButtonHighlightIssue/ViewController.m
+++ ASDKButtonHighlightIssue/ViewController.m
@@ -8,22 +8,57 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
+@interface MyNode: ASDisplayNode
+@property (weak, nonatomic) ASViewController *parentVC;
+@property (strong, nonatomic) ASButtonNode *buttnoNode;
+@end
+
+@interface ViewController ()
 @end
 
 @implementation ViewController
+- (instancetype)init {
+    MyNode *rootNode = [[MyNode alloc] init];
+    rootNode.parentVC = self;
+    self = [super initWithNode:rootNode];
+    
+    return self;
+}
+@end
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+@implementation MyNode
+
+- (instancetype)init {
+    self = [super init];
+    
+    if (self) {
+        self.automaticallyManagesSubnodes = YES;
+        
+        _buttnoNode = [[ASButtonNode alloc] init];
+        [_buttnoNode setTitle:@"Normal" withFont:nil withColor:[UIColor blueColor] forState:UIControlStateNormal];
+        [_buttnoNode setTitle:@"Highlighted" withFont:nil withColor:[UIColor redColor] forState:UIControlStateHighlighted];
+        
+        // Important: Bug reproduced on TouchDown event, on TouchUpInside everything OK.
+        [_buttnoNode addTarget:self action:@selector(pushAnotherVC) forControlEvents:ASControlNodeEventTouchDown];
+        
+        _buttnoNode.style.flexShrink = YES;
+        
+    }
+    return self;
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(ASLayoutSpec *)layoutSpecThatFits:(ASSizeRange)constrainedSize {
+    return [[ASCenterLayoutSpec alloc] initWithHorizontalPosition:ASRelativeLayoutSpecPositionCenter
+                                                 verticalPosition:ASRelativeLayoutSpecPositionCenter
+                                                     sizingOption:ASRelativeLayoutSpecSizingOptionDefault
+                                                            child:_buttnoNode];
 }
 
+         
+- (void)pushAnotherVC {
+    ViewController *vc = [[ViewController alloc] init];
+    [_parentVC.navigationController pushViewController:vc animated:YES];
+}-A
 
 @end
